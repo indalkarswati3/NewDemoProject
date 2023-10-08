d27aae082cfaa29c18f189ea7e5852c686b6237c
indalkarswati3@gmail.com <indalkarswati3>
2023-10-08 19:38:17 +0530

    message-my first commit

---
+++ src/hollywood/A.java
@@ -0,0 +1,10 @@
+package hollywood;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("zukega nhi sala....");
+
+	}
+
+}
---
+++ src/hollywood/B.java
@@ -0,0 +1,11 @@
+package hollywood;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("badal barsa bijuli...");
+
+	}
+
+}
